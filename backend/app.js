c74a36c2044d5ad0ed267e43b78002e1baf60b71
Arron Wilcock <Arron.wilcock@outlook.com>
2023-04-17 10:29:29 +0100

    project initialized and express & mongoose added

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://arronwilcock:aNY9w8Blxag3dVos@project6cluster.inokijf.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("successfully connected to mongo db atlas");
+  })
+  .catch((error) => {
+    console.log("unable to connect to mongo db atlas");
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
